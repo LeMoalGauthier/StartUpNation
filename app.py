e894d667376125cc08088da6757f86e3f2a02f91
LeMoalGauthier <gauthier.le-moal@isen-ouest.yncrea.fr>
2025-03-21 14:03:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import subprocess
 from controllers.route import app
 
 if __name__ == '__main__':
+    # Lancer la sous-application en arrière-plan
+    subprocess.Popen(['python', 'sous_app/app.py'])
+    
+    # Lancer l'application principale
     app.run(debug=True)
